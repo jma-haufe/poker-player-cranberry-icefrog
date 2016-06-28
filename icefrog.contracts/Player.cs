78dec092761e8282f3e398c6bd55d96e02d38772
Daniel Hampe <daniel.hampe@haufe-lexware.com>
2016-06-28 12:33:23 +0200

    Added properties to Player

--- icefrog.contracts/Player.cs
+++ icefrog.contracts/Player.cs
@@ -1,7 +1,26 @@
-﻿namespace Icefrog
+﻿using System.Collections.Generic;
+
+namespace Icefrog
 {
     public class Player
-    {
-        
+    { 
+        IEnumerable<Card> _holeCards = new List<Card>();
+
+        public int Bet { get; set; }
+
+        public IEnumerable<Card> HoleCards
+        {
+            get { return _holeCards; }
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Stack { get; set; }
+
+        public string Status { get; set; }
+
+        public string Version { get; set; }
     }
 }
