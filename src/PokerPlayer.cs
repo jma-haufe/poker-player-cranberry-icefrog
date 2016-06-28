57930d02df7a2f61cbd2ff820bcb488abd124a5a
Jochen Mader <Jochen.Mader@haufe-lexware.com>
2016-06-28 11:04:06 +0200

    simple bet

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -5,21 +5,22 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Default C# folding player";
+		public static readonly string VERSION = "Cranberry Icefrogs Pokerbot";
+
+        public static RainmanWrapper Rainman = new RainmanWrapper();
 
 		public static int BetRequest(JObject gameState)
 		{
             int bet = 0;
             var smallBlind = gameState.Value<int>("small_blind");
-
-            bet = smallBlind * 2;
-
+            var currentBuyIn = gameState.Value<int>("current_buy_in");
+            bet = currentBuyIn + smallBlind * 2;
+            
             return bet;
 		}
         
         public static void ShowDown(JObject gameState)
 		{
-			//TODO: Use this method to showdown
 		}
 	}
 }
---
+++ src/RainmanWrapper.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace Nancy.Simple
+{
+    public class RainmanWrapper : NancyModule
+    {
+        public Nancy.Response GetInfo()
+        {
+            Get["/"] = _ => {
+                var contentBytes = Encoding.UTF8.GetBytes("OK");
+                var response = new Response
+                {
+                    ContentType = "text/plain",
+                    Contents = s => s.Write(contentBytes, 0, contentBytes.Length),
+                    StatusCode = HttpStatusCode.OK
+                };
+                return response;
+            };
+            return null;
+        }
+
+    }
+}
