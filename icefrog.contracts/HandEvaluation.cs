93f3a1c4ec3466f9bdf98e67724f9bec31d5799b
Daniel Hampe <daniel.hampe@haufe-lexware.com>
2016-06-28 15:11:29 +0200

    Added some more evaluation for "same of a kind" - cards

--- icefrog.contracts/HandEvaluation.cs
+++ icefrog.contracts/HandEvaluation.cs
@@ -4,26 +4,74 @@ namespace Icefrog
 {
     public class HandEvaluation
     {
-        public IEnumerable<Card> HoleCards { get; set; }
+        private List<Card> AllCards
+        {
+            get { return JoinCards(HoleCards, CommunityCards); }
+        }
+
         public IEnumerable<Card> CommunityCards { get; set; }
+        public IEnumerable<Card> HoleCards { get; set; }
 
         public bool IsOnePair
         {
             get
             {
-                var cards = new List<Card>();
-                cards.AddRange(HoleCards);
-                cards.AddRange(CommunityCards);
-
-                for (int i = 0; i < cards.Count; i++)
-                {
-                    var card = cards[0];
-                    return false;
-                }
-
-
-                return false;
+                var cards = JoinCards(HoleCards, CommunityCards);
+                return HasSameOfAKind(cards, 2);
             }
+        }
+        public bool IsThreeOfAKind
+        {
+            get
+            {
+                var cards = JoinCards(HoleCards, CommunityCards);
+                return HasSameOfAKind(cards, 3);
+            }
+        }
+
+        public bool IsFourOfAKind
+        {
+            get
+            {
+                var cards = AllCards;
+                return HasSameOfAKind(cards, 4);
+            }
+        }
+
+        private static bool HasSameOfAKind(List<Card> cards, int sameKind)
+        {
+            int sameRank = 0;
+            foreach (var leftCard in cards)
+            {
+                foreach (var rightCard in cards)
+                {
+                    if (rightCard == leftCard)
+                    {
+                        continue;
+                    }
+
+                    if (rightCard.Rank == leftCard.Rank)
+                    {
+                        sameRank++;
+                    }
+
+                    if (sameRank == sameKind)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static List<Card> JoinCards(params IEnumerable<Card>[] cardCollections)
+        {
+            var joinedCards = new List<Card>();
+            foreach (var cardCollection in cardCollections)
+            {
+                joinedCards.AddRange(cardCollection);
+            }
+            return joinedCards;
         }
     }
 }
